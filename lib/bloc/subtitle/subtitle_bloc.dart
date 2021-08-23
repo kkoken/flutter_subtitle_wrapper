299e48d027440b17004a2f6c8665de4f52a7b242
Kaan Taha Koken <kaantahakoken@Kaan-MacBook-Pro.local>
2021-08-23 15:38:04 +0300

    video_player replaced with better_player

--- lib/bloc/subtitle/subtitle_bloc.dart
+++ lib/bloc/subtitle/subtitle_bloc.dart
@@ -1,25 +1,25 @@
 import 'dart:async';
 
+import 'package:better_player/better_player.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:subtitle_wrapper_package/data/models/subtitle.dart';
 import 'package:subtitle_wrapper_package/data/models/subtitles.dart';
 import 'package:subtitle_wrapper_package/data/repository/subtitle_repository.dart';
 import 'package:subtitle_wrapper_package/subtitle_controller.dart';
-import 'package:video_player/video_player.dart';
 
 part 'subtitle_event.dart';
 part 'subtitle_state.dart';
 
 class SubtitleBloc extends Bloc<SubtitleEvent, SubtitleState> {
-  final VideoPlayerController videoPlayerController;
+  final BetterPlayerController controller;
   final SubtitleRepository subtitleRepository;
   final SubtitleController subtitleController;
 
   late Subtitles subtitles;
 
   SubtitleBloc({
-    required this.videoPlayerController,
+    required this.controller,
     required this.subtitleRepository,
     required this.subtitleController,
   }) : super(SubtitleInitial()) {
@@ -47,14 +47,13 @@ class SubtitleBloc extends Bloc<SubtitleEvent, SubtitleState> {
 
   Stream<SubtitleState> loadSubtitle() async* {
     yield LoadingSubtitle();
-    videoPlayerController.addListener(
+    final videoPlayerController = controller.videoPlayerController;
+    videoPlayerController?.addListener(
       () {
         final videoPlayerPosition = videoPlayerController.value.position;
         for (final Subtitle subtitleItem in subtitles.subtitles) {
-          final bool validStartTime = videoPlayerPosition.inMilliseconds >
-              subtitleItem.startTime.inMilliseconds;
-          final bool validEndTime = videoPlayerPosition.inMilliseconds <
-              subtitleItem.endTime.inMilliseconds;
+          final bool validStartTime = videoPlayerPosition.inMilliseconds > subtitleItem.startTime.inMilliseconds;
+          final bool validEndTime = videoPlayerPosition.inMilliseconds < subtitleItem.endTime.inMilliseconds;
           if (validStartTime && validEndTime) {
             add(
               UpdateLoadedSubtitle(
--- lib/subtitle_wrapper_package.dart
+++ lib/subtitle_wrapper_package.dart
@@ -1,5 +1,6 @@
 library subtitle_wrapper_package;
 
+import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:subtitle_wrapper_package/bloc/subtitle/subtitle_bloc.dart';
@@ -7,12 +8,11 @@ import 'package:subtitle_wrapper_package/data/models/style/subtitle_style.dart';
 import 'package:subtitle_wrapper_package/data/repository/subtitle_repository.dart';
 import 'package:subtitle_wrapper_package/subtitle_controller.dart';
 import 'package:subtitle_wrapper_package/subtitle_text_view.dart';
-import 'package:video_player/video_player.dart';
 
 class SubTitleWrapper extends StatelessWidget {
   final Widget videoChild;
   final SubtitleController subtitleController;
-  final VideoPlayerController videoPlayerController;
+  final BetterPlayerController videoPlayerController;
   final SubtitleStyle subtitleStyle;
 
   const SubTitleWrapper({
@@ -36,7 +36,7 @@ class SubTitleWrapper extends StatelessWidget {
             right: subtitleStyle.position.right,
             child: BlocProvider(
               create: (context) => SubtitleBloc(
-                videoPlayerController: videoPlayerController,
+                controller: videoPlayerController,
                 subtitleRepository: SubtitleDataRepository(
                   subtitleController: subtitleController,
                 ),
